fbc5e4b93e2188d458f886f2928fa9913a380010
Lucas Gabriel <lucasgralcantara@gmail.com>
2020-09-09 16:42:29 -0300

    Funcionalidade: Implementadas funções de criação e gerenciamento de posts

--- APIblog.js
+++ APIblog.js
@@ -21,6 +21,7 @@ const autor = {
     senha:
     deletado:
 } */
+
 const Koa = require("koa");
 const bodyparser = require("koa-bodyparser");
 
@@ -30,16 +31,17 @@ server.use(bodyparser());
 
 const autores = [];
 const posts = [];
-let index = 0;
+let indexAutor = 0;
+let indexPost = 0;
 
 const criarAutor = (body) => {
     if (autores.length === 0) {
-        index = 1;
+        indexAutor = 1;
     } else {
-        index = autores[autores.length-1].id + 1;
+        indexAutor = autores[autores.length-1].id + 1;
     }
     const novoAutor = {
-        id: index,
+        id: indexAutor,
         primeiroNome: body.primeiroNome,
         ultimoNome: body.ultimoNome,
         email: body.email,
@@ -51,9 +53,9 @@ const criarAutor = (body) => {
 }
 
 const obterAutor = (id) => {
-    for (item of autores) {
-        if (item.id == id) {
-            return item
+    for (const autor of autores) {
+        if (autor.id == id) {
+            return autor
         }
     }
 }
@@ -65,6 +67,7 @@ const obterAutores = () => {
 const removerAutor = (id) => {
     const autor = obterAutor(id);
     autor.deletado = true;
+    //Verificar se existe algum post com esse autor
 }
 
 const atualizaAutor = (id, propriedade, valor) => {
@@ -79,6 +82,61 @@ const atualizaAutor = (id, propriedade, valor) => {
         autores[index].email = valor;
     } else if (propriedade === 'senha') {
         autores[index].senha = valor;
+    }
+}
+
+const criarPost = (body) => {
+    //Não deve existir sem um autor
+    //Não deve ser publicado com autor deletado
+    if (posts.length === 0) {
+        indexPost = 1;
+    } else {
+        indexPost = posts[posts.length-1].id + 1;
+    }
+
+    const novoPost = {
+        id: indexPost,
+        titulo: body.titulo,
+        subtitulo: body.subtitulo,
+        autor: body.autor,
+        conteudo: body.conteudo,
+        publicado: false,
+        deletado: false
+    }
+
+    posts.push(novoPost);
+}
+
+const obterPost = (id) => {
+    for (const post of posts) {
+        if (post.id == id) {
+            return post
+        }
+    }
+}
+
+const obterPosts = () => {
+    return posts.reverse();
+}
+
+const removerPost = (id) => {
+    const post = obterAutor(id);
+    post.deletado = true;
+    //Verificar se existe algum post com esse autor
+}
+
+const atualizaPost = (id, propriedade, valor) => {
+    const post = obterPost(id)
+    const index = autores.indexOf(autor);
+
+    if (propriedade === 'titulo') {
+        post[index].titulo = valor;
+    } else if (propriedade === 'subtitulo') {
+        post[index].subtitulo = valor;
+    } else if (propriedade === 'autor') {
+        post[index].autor = valor;
+    } else if (propriedade === 'conteudo') {
+        post[index].conteudo = valor;
     }
 }
 
@@ -112,16 +170,24 @@ server.use((ctx) => {
 
     if (path.includes("/posts")) {
         if (method === 'GET') {
-
+            if (idRequisicao) {
+                ctx.body = obterPost(idRequisicao);
+            } else ctx.body = obterPosts();
         } else 
         if (method === 'POST') {
-
+            criarPost(ctx.request.body);
+            //Se já existir?
+            //Verificar erros da função
         } else 
         if (method === 'DELETE') {
-
+            if (idRequisicao) {
+                removerPost(idRequisicao);
+            }//E se não tiver?
         } else 
         if (method === 'PUT') {
-
+            if (idRequisicao) {
+                atualizaAutor(idRequisicao, ctx.request.body.propriedade, ctx.request.body.valor);
+            }//E se der errado?
         }
     }
 })
